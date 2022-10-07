4ae934036fabb08324dd5c1eae3806f13306f1ac
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-10-07 18:38:08 -0300

    Update 7-b.c

--- 7-b.c
+++ 7-b.c
@@ -3,11 +3,13 @@
 
 
 int main(){
+    /*inicializando as variáveis*/
     int Tamanho;
+       /*interação com o usuário*/
     printf("Qual o tamanho do quadrado? ");
     scanf("%d",&Tamanho);
 
-
+     /*primeiro laço de repetição fica responsável pela linha, e a segunda pela coluna*/
     for(int i = 0 ; i < Tamanho; i++){
             printf("\n\n\t");
         for(int i = 0 ; i < Tamanho; i++){
