4983acf2006fdec2b93b5ad421ed5cdd9ba43de3
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-10-07 18:37:17 -0300

    Update 7-a.c

--- 7-a.c
+++ 7-a.c
@@ -3,7 +3,7 @@
 
 
 int main(){
-
+     /*primeiro laço de repetição fica responsável pela linha, e a segunda pela coluna*/
     for(int i = 0 ; i < 5; i++){
             printf("\n\n\t");
         for(int i = 0 ; i < 5; i++){
