0b2de6c5d4aa25bc23f87e20ffdcd9ae1f6f6376
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-10-08 19:28:51 -0300

    Update 9-a.c

--- 9-a.c
+++ 9-a.c
@@ -3,10 +3,11 @@
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
-
+   /*interação com o usuário*/
     char nome[200];
-
+       /*interação com o usuário*/
     printf("Digite o seu nome: ");
     gets(nome);
+     /*apresentação dos resultados*/
     printf("o seu nome %s",nome);
 }
