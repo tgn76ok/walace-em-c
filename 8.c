dcff4059f5720bbe659b10c4929d791e7ace02a4
thiago.tgn <62727856+tgn76ok@users.noreply.github.com>
2022-10-08 19:28:13 -0300

    Update 8.c

--- 8.c
+++ 8.c
@@ -4,29 +4,30 @@
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
-
+    /*inicializando as vari√°veis*/
     int num,num2;
     int preco;
 
+       /*intera√ß√£o com o usu√°rio*/
 
-
-        printf("Digite um o preÁo do produto: ");
+        printf("Digite um o pre√ßo do produto: ");
         scanf("%d",&num);
 
-        printf("Digite a venda mÈdia mensal do produto: ");
+        printf("Digite a venda m√©dia mensal do produto: ");
         scanf("%d",&num2);
 
+/*processando os dados recebido pelos usuarios e mostrando o resultado na tela*/
         if(num<30 && num2<500){
             preco = num * 1.1;
-            printf("o novo preÁo do produto È %d\n",preco);
+            printf("o novo pre√ßo do produto √© %d\n",preco);
 
         }else if( (80 > num) && (num >=30) && (500 <= num2 <= 1200)){
             preco = num * 1.2;
-            printf("o novo preÁo do produto È %d\n",preco);
+            printf("o novo pre√ßo do produto √© %d\n",preco);
 
         }else if(num2 >= 1200 &&num >=80 ){
             preco = num -( num*0.2);
-            printf("o novo preÁo do produto È %d\n",preco);
+            printf("o novo pre√ßo do produto √© %d\n",preco);
 
         }else{
             printf("Nao encaixa na tabela");
